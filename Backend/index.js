b0e3edb50ce2157a76f39dd0455623debbad60e8
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-01 15:54:34 -0300

    Inserir o corpo do site

---
+++ Backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const expressMongoDb = require('express-mongo-db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ObjectID = require('mongodb').ObjectID;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
