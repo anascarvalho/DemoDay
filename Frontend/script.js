5d2548a5af31d3f79e7c6a93172546da8612d205
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-04 22:56:14 -0300

    Acrescentar responsividade no menu

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,12 +1,22 @@
 
 
-//const menu = document.querySelector(".lista");
+const menu = document.querySelector(".lista");
 const botao = document.querySelector("#hamburguer");
+const nav = document.querySelector("nav");
 
 
-function acao(){
-    ('.lista').removeClass("none")
-        
+ 
+
+function acaobotao(){
+    if(menu.classList.contains("active")){
+        menu.classList.remove("active");
+        nav.classList.remove("active");
+    }
+    else{
+        menu.classList.add("active");
+        nav.classList.add("active");
+    }
+    
 }
 
-botao.onclick(acao());
+botao.onclick = acaobotao;
